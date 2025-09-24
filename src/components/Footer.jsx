79a5ff4afacb1fed4c30919c26a07069db177612
Tarik <t2.macben@gmail.com>
2025-09-24 15:54:10 +0800

    Corrections effectuées sur la plupart des fichiers.jsx. Liens internes et externes ajoutés, et modifications du <main> dans la <section>.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,36 +3,38 @@ import '../assets/css/Footer.css'
 
 export default function Footer() {
   return (
-    <footer className=" bg-primary">
-        <Container fluid className='w-75 mx-auto text-light'>
-            <Row className="p-3">
+    <footer style={{ backgroundColor: "#3251a6ff" }}>
+        <Container fluid className='w-75 mx-auto text-light fs-5 p-4'>
+            <Row className="">
                 <Col md={4}>
                     <h4 className="footer-name">Tarik Benabbou Mac Veigh</h4>
                     <ul className="list-unstyled footer-item">
-                        <li>4 rue Arthur Rimbaud</li>
-                        <li>59139, Wattignies, France</li>
-                        <li>07 44 86 10 56</li>
-                        <li>tarik.bmacveigh@hotmail.com</li>
+                        <li>🏠 4 rue Arthur Rimbaud</li>
+                        <li>🏠 59139, Wattignies, France</li>
+                        <li>📱 07 44 86 10 56</li>
+                        <li><a className="text-decoration-none text-light" href="mailto:tarik.bmacveigh@hotmail.com">📧 tarik.bmacveigh@hotmail.com</a></li>
                     </ul>
                 </Col>
 
                 <Col md={4}>
                     <h4 className="footer-name">Pages</h4>
-                    <ul className="footer-item">
-                        <li>Accueil</li>
-                        <li>Compétences</li>
-                        <li>Projets</li>
-                        <li>Contact</li>
+                    <ul className="list-unstyled footer-item">
+                        <li><a href="/" className="text-decoration-none text-light">Accueil</a></li>
+                        <li><a href="/competences" className="text-decoration-none text-light">Compétences</a></li>
+                        <li><a href="/projects" className="text-decoration-none text-light">Projets</a></li>
+                        <li><a href="/contact" className="text-decoration-none text-light">Contact</a></li>
                     </ul>
                 </Col>
 
                 <Col md={4}>
                     <h4 className="footer-name">Mes projets personnels</h4>
-                    <ul className="footer-item">
-                        <li>GameBoy counter (HTML-CSS-JavaScript)</li>
-                        <li>Gusto e Tradizione (HTML-CSS)</li>
-                        <li>Photo album (Node.js)</li>
-                        <li>Liste de jeux (Python)</li>
+                    <ul className="list-unstyled footer-item">
+                        <li><a className="text-decoration-none text-light" href="https://github.com/tarik-benabbou-mac-veigh/Game-Boy-Counter" target="_blank" rel="noopener noreferrer">🎮 GameBoy counter (HTML/CSS/JavaScript)</a></li>
+                        <li><a className="text-decoration-none text-light" href="https://github.com/tarik-benabbou-mac-veigh/Restaurant-Guso-e-Tradizione" target="_blank" rel="noopener noreferrer">🍽️ Gusto e Tradizione (HTML/CSS)</a></li>
+                        <li><a className="text-decoration-none text-light" href="https://github.com/tarik-benabbou-mac-veigh/liste_jeux" target="_blank" rel="noopener noreferrer">📋 Liste de jeux (Python)</a></li>
+                        <li><a className="text-decoration-none text-light" href="https://github.com/tarik-benabbou-mac-veigh/tifosi_database" target="_blank" rel="noopener noreferrer">📊 Tifosi database (SQL)</a></li>
+                        <li><a className="text-decoration-none text-light" href="https://github.com/tarik-benabbou-mac-veigh/photo-album" target="_blank" rel="noopener noreferrer">📸 Photo album (Node.js)</a></li>
+                        <li><a className="text-decoration-none text-light" href="https://github.com/tarik-benabbou-mac-veigh/BTC-live-exchange-rate" target="_blank" rel="noopener noreferrer">📈 Bitcoin cours (JavaScript)</a></li>
                     </ul>
                 </Col>
             </Row>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,18 @@ import '../assets/css/Header.css'
 export default function Header() {
   return (
     <div>
-        <Navbar bg="primary" data-bs-theme="dark" expand="lg">
-          <Container fluid className='w-75 mx-auto'>
-            <Navbar.Brand as={Link} to="/" className='nav-logo'>Tarik - Portfolio</Navbar.Brand>
-              <Nav className="ms-auto">
-                <Link as={Link} to="/" className='text-light py-3 px-4 rounded-3 nav-link'>Accueil</Link>
-                <Link as={Link} to="/competences" className='text-light py-3 px-4 rounded-3 nav-link'>Compétences</Link>
-                <Link as={Link} to="/projects" className='text-light py-3 px-4 rounded-3 nav-link'>Projets</Link>
-                <Link as={Link} to="/contact" className='text-light py-3 px-4 rounded-3 nav-link'>Contact</Link>
-              </Nav>
-          </Container>
-        </Navbar>
+      <Navbar style={{ backgroundColor: "#3251a6ff" }} data-bs-theme="dark" expand="lg">
+        <Container fluid className='w-75 mx-auto'>
+          <Navbar.Brand as={Link} to="/" className='nav-logo'>Tarik - Portfolio</Navbar.Brand>
+          <Nav className="ms-auto">
+            <Link as={Link} to="/" className='fs-5 text-light py-3 px-4 rounded-3 nav-link'>Accueil</Link>
+            <Link as={Link} to="/competences" className='fs-5 text-light py-3 px-4 rounded-3 nav-link'>Compétences</Link>
+            <Link as={Link} to="/projects" className='fs-5 text-light py-3 px-4 rounded-3 nav-link'>Projets</Link>
+            <Link as={Link} to="/contact" className='fs-5 text-light py-3 px-4 rounded-3 nav-link'>Contact</Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
     </div>
   )
 }
