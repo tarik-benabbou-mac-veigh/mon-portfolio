819a14f8128d9086625bf965eb7fc24032151a4e
Tarik <t2.macben@gmail.com>
2025-10-03 16:05:51 +0800

    Code original ajouté de nouveau dans App.jsx pour vérification

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,20 @@
-// Fichier: src/App.jsx (TEMPORAIRE)
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route, Link } from 'react-router-dom';
-// ... autres imports commentés
+import Home from './pages/home';
+import Competences from './pages/Competences';
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <>
       <div>
-        <h1>✅ TEST DE DÉPLOIEMENT RÉUSSI !</h1>
-        {/* <Routes>
+        <Routes>
           <Route path='/' element={<Home/>}></Route>
           <Route path='/competences' element={<Competences/>}></Route>
           <Route path='/projects' element={<Projects/>}></Route>
-        </Routes> */}
+        </Routes>
       </div>
     </>
   )
 }
 export default App
-
-
-
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Routes, Route, Link } from 'react-router-dom';
-// import Home from './pages/home';
-// import Competences from './pages/Competences';
-// import Projects from './pages/Projects';
-
-// function App() {
-//   return (
-//     <>
-//       <div>
-//         <Routes>
-//           <Route path='/' element={<Home/>}></Route>
-//           <Route path='/competences' element={<Competences/>}></Route>
-//           <Route path='/projects' element={<Projects/>}></Route>
-//         </Routes>
-//       </div>
-//     </>
-//   )
-// }
-// export default App
