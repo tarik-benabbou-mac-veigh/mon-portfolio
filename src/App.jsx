94b843ce514a863e386cc11141f0ccbaf7434816
Tarik <t2.macben@gmail.com>
2025-10-03 15:43:37 +0800

    Test rendu du contenu statique après HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
+// Fichier: src/App.jsx (TEMPORAIRE)
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/home';
-import Competences from './pages/Competences';
-import Projects from './pages/Projects';
+// ... autres imports commentés
 
 function App() {
-
   return (
     <>
       <div>
-
-        <Routes>
+        <h1>✅ TEST DE DÉPLOIEMENT RÉUSSI !</h1>
+        {/* <Routes>
           <Route path='/' element={<Home/>}></Route>
           <Route path='/competences' element={<Competences/>}></Route>
           <Route path='/projects' element={<Projects/>}></Route>
-        </Routes>
+        </Routes> */}
       </div>
     </>
   )
 }
-
 export default App
+
+
+
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Routes, Route, Link } from 'react-router-dom';
+// import Home from './pages/home';
+// import Competences from './pages/Competences';
+// import Projects from './pages/Projects';
+
+// function App() {
+//   return (
+//     <>
+//       <div>
+//         <Routes>
+//           <Route path='/' element={<Home/>}></Route>
+//           <Route path='/competences' element={<Competences/>}></Route>
+//           <Route path='/projects' element={<Projects/>}></Route>
+//         </Routes>
+//       </div>
+//     </>
+//   )
+// }
+// export default App
